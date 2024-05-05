c4055b75cb52b24cf36259ca353004cc152f1a70
SubordinalBlue <friday.dietrich@gmail.com>
2024-05-05 13:05:10 +0900

    Cleanup after rebase; fix for <columns> with border

--- common/src/main/java/earth/terrarium/hermes/api/defaults/columns/ColumnsTagElement.java
+++ common/src/main/java/earth/terrarium/hermes/api/defaults/columns/ColumnsTagElement.java
@@ -21,25 +21,25 @@ public class ColumnsTagElement extends FillAndBorderElement implements TagElemen
             String template = parameters.get("template");
             this.colSpecs = new ArrayList<>(Arrays.asList(template.split(" ")));
         }
+        // Valid column specifications (colSpecs) are:
+        // - An integer, possibly followed by a '%'. Always interpreted as a percent of the document width.
+        // - '*': All columns specified this way share equally in any remaining, unspecified width.
+        // - '@': fit (shrink) to contents.
+        //      Items, images, and entities have determinable widths.
+        //      TextTag elements (and therefore <p>, <h1>, and <h2>) specify a width with the 'minWidth' attribute.
+        //      All other cases default to a 0 width; providing visual feedback in a likely 'wrong', layout result.
     }
 
     @Override
     public void render(Theme theme, GuiGraphics graphics, int x, int y, int width, int mouseX, int mouseY, boolean hovered, float partialTicks) {
 
-        // Valid column specifications (colSpecs) are:
-        // - An integer, possibly followed by a '%'. Always interpreted as a percent of the document width.
-        // - '@': fit (shrink) to contents.
-        //      Currently only items, images, and entities have determinable widths.
-        //      TextTag elements (and therefore <p>, <h1>, and <h2>) specify a width with the 'minWidth' attribute.
-        //      All other cases default to a 0 width; providing visual feedback in likely 'wrong', layout result.
-        // - '*': All columns specified this way share equally in the remaining space.
+        int contentWidth = width - (2 * xSurround);
+        int contentHeight = this.getHeight(contentWidth) - (2 * ySurround);
+        drawFillAndBorder(graphics, x + xSurround, y + ySurround, contentWidth, contentHeight);
 
-        // colSpecs, colSpecs, and children are all assumed to be the same size at this point; thanks to addChild().
-        // colSpecs have been default set to '*', unless user-supplied via the 'template' attribute.
-        // colWidths have been default set to 0
-        // Now we set all colWidths according to their corresponding colSpecs.
-        // '@' specs necessitate looking up matching children's widths.
-
+        // colWidths set or calculated from their corresponding colSpecs.
+        // We do this here once; not all needed information is available before hand.
+        // '@' specs necessitate looking up children's widths.
         if (!colWidthsCalculated) {
             int sumKnown = 0;
             int countExpands = 0;
@@ -53,13 +53,13 @@ public class ColumnsTagElement extends FillAndBorderElement implements TagElemen
                             int percent = colSpec.endsWith("%")
                                     ? Integer.parseInt(colSpec.substring(0, colSpec.length() - 1))
                                     : Integer.parseInt(colSpec);
-                            colWidths.set(i, Math.round((percent / 100f) * width));
+                            colWidths.set(i, Math.round((percent / 100f) * contentWidth));
                         }
                     }
                 }
                 sumKnown += colWidths.get(i);
             }
-            int expandWidth = Math.round((width - sumKnown) / (float) countExpands);
+            int expandWidth = Math.round((contentWidth - sumKnown) / (float) countExpands);
             for (int i = 0; i < colWidths.size(); i++) {
                 if (colSpecs.get(i).equals("*")) {
                     colWidths.set(i, expandWidth);
@@ -67,10 +67,6 @@ public class ColumnsTagElement extends FillAndBorderElement implements TagElemen
             }
             colWidthsCalculated = true;
         }
-
-        int contentWidth = width - (2 * xSurround);
-        int contentHeight = this.getHeight(contentWidth) - (2 * ySurround);
-        drawFillAndBorder(graphics, x + xSurround, y + ySurround, contentWidth, contentHeight);
 
         for (int i = 0; i < children.size(); i++) {
             var child = children.get(i);
@@ -116,6 +112,7 @@ public class ColumnsTagElement extends FillAndBorderElement implements TagElemen
             throw new IllegalArgumentException("<columns> can only contain <column> elements.");
         }
         this.children.add(element);
+        // colSpecs (default '*'), and colWidths (default 0), should be the same size as children
         if (colSpecs.size() < children.size()) { colSpecs.add("*"); }
         colWidths.add(0);
     }
